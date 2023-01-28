5832812d6db4d9bbb4df3821f9419a5f30bb7817
HIFZUR RAHMAN <97081829+hifzur001@users.noreply.github.com>
2023-01-28 16:55:19 +0530

    Add files via upload

---
+++ MacBookAir1.js
@@ -0,0 +1,138 @@
+import "./MacBookAir1.css";
+import Ellipse from "./Ellipse 1238.png";
+import Mask from "./Mask group.png";
+import User from "./image 897.png";
+import Folder from "./image 898.png";
+import Upload from "./image 899.png";
+import Report from "./image 900.png";
+import Setting from "./image 901.png";
+import LogOut from "./image 902.png";
+import Note from "./note 4.png";
+import Vector from "./Vector 739.png";
+import Notes from "./note 2.png";
+const MacBookAir1 = () => {
+  return (
+    <div className="box">
+      <div className="macbook-air-1">
+        <img
+          className="macbook-air-1-child"
+          alt=""
+          src={Ellipse}
+        />
+        <b className="diane-cooper">Diane Cooper</b>
+        <div className="div">15</div>
+        <div className="div1">02</div>
+        <div className="female">Female</div>
+        <div className="jl-diponegoro-no">JL. Diponegoro No. 21</div>
+        <div className="active-member">Active Member</div>
+        <div className="feb-24th-1997">Feb 24th, 1997</div>
+        <div className="cilacap">Cilacap</div>
+        <div className="feb-24th-19971">Feb 24th, 1997</div>
+        <div className="div2">(239) 555 -0108</div>
+        <div className="div3">655849</div>
+        <div className="dianecooperexamplecom">diane.cooper@example.com</div>
+        <div className="past">Past</div>
+        <div className="upcoming">Upcoming</div>
+        <div className="gender">Gender</div>
+        <div className="street-address">Street Address</div>
+        <div className="member-status">Member Status</div>
+        <div className="registered-date">Registered Date</div>
+        <div className="city">City</div>
+        <div className="birthday">Birthday</div>
+        <div className="phone-number">Phone Number</div>
+        <div className="zip-code">Zip Code</div>
+        <img className="macbook-air-1-item" alt="" src="../vector-735.svg" />
+        <img className="macbook-air-1-inner" alt="" src="../vector-739.svg" />
+        <img className="vector-icon" alt="" src="../vector-739.svg" />
+        <img className="macbook-air-1-child1" alt="" src={Vector} />
+        <img className="macbook-air-1-child2" alt="" src={Vector} />
+        <img className="macbook-air-1-child3" alt="" src={Vector} />
+        <img className="macbook-air-1-child4" alt="" src={Vector} />
+        <img className="macbook-air-1-child5" alt="" src={Vector} />
+        <img className="macbook-air-1-child6" alt="" src={Vector} />
+        <div className="send-message-wrapper">
+          <b className="send-message">Send Message</b>
+        </div>
+        <div className="frame-parent">
+          <div className="upcoming-appointments-wrapper">
+            <div className="send-message">Upcoming Appointments</div>
+          </div>
+          <div className="past-appointments-wrapper">
+            <div className="past-appointments">Past Appointments</div>
+          </div>
+          <div className="past-appointments-wrapper">
+            <div className="past-appointments">Medical Records</div>
+          </div>
+        </div>
+        <div className="rectangle-div" />
+        <div className="root-canal-treatment">Root Canal Treatment</div>
+        <div className="show-previous-treatments-wrapper">
+          <div className="past-appointments">Show Previous Treatments</div>
+        </div>
+        <div className="macbook-air-1-child7" />
+        <div className="div4">09.00 - 10.00</div>
+        <b className="nov-19">26 Nov ‘19</b>
+        <div className="open-access">Open Access</div>
+        <div className="drg-adam-h">Drg. Adam H.</div>
+        <div className="jessicamila">Jessicamila</div>
+        <div className="treatment">Treatment</div>
+        <div className="dentist">Dentist</div>
+        <div className="nurse">Nurse</div>
+        <div className="note">Note</div>
+        <img className="macbook-air-1-child8" alt="" src={Notes} />
+        <img className="macbook-air-1-child9" alt="" src={Notes} />
+        <img className="note-2-icon" alt="" src={Notes} />
+        <div className="macbook-air-1-child10" />
+        <div className="div5">09.00 - 10.00</div>
+        <b className="nov-191">12 Nov ‘19</b>
+        <div className="root-canal">Root Canal</div>
+        <div className="drg-adam-h1">Drg. Adam H.</div>
+        <div className="jessicamila1">Jessicamila</div>
+        <div className="treatment1">Treatment</div>
+        <div className="dentist1">Dentist</div>
+        <div className="nurse1">Nurse</div>
+        <div className="note1">Note</div>
+        <img className="macbook-air-1-child11" alt="" src="../vector-749.svg" />
+        <img className="macbook-air-1-child12" alt="" src="../vector-750.svg" />
+        <img className="note-3-icon" alt="" src={Notes} />
+        <img className="macbook-air-1-child13" alt="" src="../vector-745.svg" />
+        <div className="files-documents">Files / Documents</div>
+        <div className="macbook-air-1-child14" />
+        <div className="macbook-air-1-child15" />
+        <div className="check-up-resultspdf">Check Up Results.pdf</div>
+        <img className="note-4-icon" alt="" src={Note} />
+        <div className="check-up-resultspdf1">Check Up Results.pdf</div>
+        <img className="note-5-icon" alt="" src={Note} />
+        <div className="macbook-air-1-child16" />
+        <div className="medical-prescriptionpdf">Medical Prescription.pdf</div>
+        <img className="note-6-icon" alt="" src={Note} />
+        <div className="macbook-air-1-child17" />
+        <div className="dental-x-ray-resultpdf">Dental X-Ray Result.pdf</div>
+        <img className="note-1-icon" alt="" src={Note} />
+        <div className="rectangle-parent">
+          <div className="group-child" />
+          <div className="group-item" />
+          <div className="new-patient-folder-container">
+            <p className="new">{`NEW `}</p>
+            <p className="new">PATIENT</p>
+            <p className="new">{`FOLDER `}</p>
+            <p className="new">{`UPLOAD `}</p>
+            <p className="new">{`REPORT `}</p>
+            <p className="new">SETTING</p>
+            <p className="new">LOGOUT</p>
+          </div>
+          <img className="mask-group-icon" alt="" src={Mask} />
+          <div className="div6">+</div>
+          <img className="image-897-icon" alt="" src={User} />
+          <img className="image-898-icon" alt="" src={Folder} />
+          <img className="image-899-icon" alt="" src={Upload} />
+          <img className="image-900-icon" alt="" src={Report} />
+          <img className="image-901-icon" alt="" src={Setting} />
+          <img className="image-902-icon" alt="" src={LogOut} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MacBookAir1;
---
+++ index.js
@@ -0,0 +1,4 @@
+import ReactDOM from 'react-dom/client'
+import MacBookAir1 from './MacBookAir1';
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<MacBookAir1 />)
